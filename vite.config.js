93e7204cbb297a77e8bb987cdef0981761523149
Satwik-create <satwiksahoo13@gmailcom>
2025-04-09 10:16:22 +0530

    github pages changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from 'vite-plugin-gh-pages'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
+  base: '/<repository-name>/', // Replace <repository-name> with your GitHub repo name
 })
